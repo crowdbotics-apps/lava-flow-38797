66684449d34de5a90fd66fceb4ec1e8fd1790ccf
Crowdbotics <team@crowdbotics.com>
2023-02-07 20:15:57 +0000

    Initial commit.

---
+++ backend/lava_flow_38797/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for lava_flow_38797 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lava_flow_38797.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lava-flow-38797",
+  url: "https://lava-flow-38797.botics.co",
+  api: "https://lava-flow-38797.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
